134cfd4b7326a0c098b46f7bd938833fcf476d35
coolKev666 <40505637+coolKev666@users.noreply.github.com>
2020-04-30 21:14:43 -0400

    Create index.js
    Created "index.js" as a sample script.

---
+++ index.js
@@ -0,0 +1,63 @@
+
+// ------------------------------------------------------------------
+// --------------------- Libraries + Variables ----------------------
+// ------------------------------------------------------------------
+// 1. express - Lightweight node.js framework for web application/APIs
+const express = require('express');
+var app = express();
+
+// 2. express.Router() - Establish API endpoint for frontend communication based on API_PORT
+const router = express.Router();
+const API_PORT = 8000;
+
+// 3. cors - Express middleware for handling http requests (from frontend mainly)
+var cors = require('cors');
+app.use(cors());
+
+// 4. cn_str - connection string to establish communication with target DB
+cn_str = "DRIVER={DB2};DATABASE=<db_name>;HOSTNAME=<host_address>;UID=<user_id>;PWD=<password>;PORT=<PORT_NO>;PROTOCOL=TCPIP";
+
+
+// ------------------------------------------------------------------
+// --------------------------- Functions ----------------------------
+// ------------------------------------------------------------------
+// Example Select Function
+router.get('/selectData', (req, res) => {
+    var ibmdb = require('ibm_db');
+    cn = cn_str
+
+    ibmdb.open(cn, function (err, db) {
+
+        if (err) return console.log(err);
+        var sql = `select * from <your_db_name>`;
+
+        db.query(sql, function (err, data) {
+            if (err) console.log(err);
+            else {
+                console.log("data fetch success");
+            }
+            db.close(function () {
+                console.log('done');
+            });
+
+            return res.json(data);
+        });
+    });
+    return ("Error - no data fetched");
+});
+
+
+// ------------------------------------------------------------------
+// ----------------- Listen for incoming requests -------------------
+// ------------------------------------------------------------------
+
+app.use('/api', router);
+app.listen(API_PORT, 'localhost', () => console.log(`LISTENING ON PORT ${API_PORT}`));
+
+
+/*
+Note - to test the API function "selectData", 
+
+open browser and run localhost:8000/api/selectData
+
+*/
